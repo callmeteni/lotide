7a96c2713701bf7889479a720c03e64fc9489ae7
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-05-22 08:10:35 +0000

    Given an array with other arrays inside, it can flatten it into a single-level array

---
+++ flatten.js
@@ -0,0 +1,26 @@
+const flatten = function (array) {
+    const flattenedArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            // check if argument is an array 
+            const nestedArray = flatten(array[i]);
+            // Then, we iterate over the elements of the nestedArray using a loop and individually push them 
+            for (let j = 0; j < nestedArray.length; j++) {
+                //if argument is an array we add the values to our array
+                flattenedArray.push(nestedArray[j]);
+            }
+        } else {
+            // If the argument is not an array, add it to the flattenedArray
+            flattenedArray.push(array[i]);
+        }
+    }
+
+    return flattenedArray;
+    // return our new array of flattened array
+};
+
+// Test
+const inputArray = [1, 2, [3, 4], 5, [6]];
+const flattenedArray = flatten(inputArray);
+console.log(flattenedArray); // [1, 2, 3, 4, 5, 6]
