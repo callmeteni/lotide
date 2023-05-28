5bc1343f0a4178b996f1ff56823266ca276011db
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-05-28 21:27:15 +0000

    countLetters('LHL') should return results indicating that L appears twice, and H once.

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+const assertEqual = function (actual, expected) {
+    if (actual === expected) {
+        return console.log(`Assertion Passed: ${actual}  ===  ${expected}`);
+
+    } else if (actual !== expected) {
+        return console.log(`Assertion Failed: ${actual}  !==  ${expected}`);
+    }
+};
+
+
+const countLetters = function(sentence) {
+// This line declares an empty object called result. This object will store the letter counts
+    let result = {};
+// loop over each character 
+    for (let char of sentence) {
+// checking if the character is not a space 
+      if (char !== ' ') {
+// checking to see if the character already exists then increments it 
+        if (result[char]) {
+          result[char]++;
+        } else {
+// else we  return the char appearred once 
+          result[char] = 1;
+        }
+      }
+    }
+  
+    return result;
+  };
+
+
+const sentence = 'LHL';
+const letterCounts = countLetters(sentence);
+console.log(letterCounts);
+console.log(countLetters("lighthouse in the house"));
+
+
