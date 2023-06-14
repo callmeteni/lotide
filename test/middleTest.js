edc538756b4db102dc74ad0fd0a7a7dff11af6cf
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-14 06:53:19 +0000

    test cases using mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
-const assertArraysEquals = require('../assertArraysEquals');
+const assert = require('chai').assert;
 const middle = require('../middle')
 
-
+describe('#middle', function() {
+    it('should return the middle element of an odd-sized array', function() {
+      const result = middle([1, 2, 3, 4]);
+      assert.deepEqual(result, [2, 3]);
+    });
+  
+    it('should return the two middle elements of an even-sized array', function() {
+      const result = middle([1, 2, 3, 4, 5, 6]);
+      assert.deepEqual(result, [3, 4]);
+    });
+  });
 
 //TEST CODE
 // console.log(middle([1, 2, 3, 4]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
+
+describe('tail', function() {
+    it('should return the tail of an array', function() {
+      const result = tail(["Hello", "Lighthouse", "Labs"]);
+      assert.deepEqual(result, ["Lighthouse", "Labs"]);
+    });
+  });
+
+
 //TEST
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
