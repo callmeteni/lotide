df6636cef6f88d9ee4c909c5e5584b5a8df9b24e
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-14 05:48:24 +0000

    refactoring using require call

--- head.js
+++ head.js
@@ -1,27 +1,9 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      return console.log(`Assertion Passed: ${actual}  ===  ${expected}`);
-  
-    } else if (actual !== expected) {
-      return console.log(`Assertion Failed: ${actual}  !==  ${expected}`);
-    }
-  };
-  
-  // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-  assertEqual(1, "sam");
-  
-  
-  //
-
-  function head(arr){
-    if(arr.length === 0){
+const assertEqual = require('./assertEqual');
+const head = function (arr) {
+  if (arr.length === 0) {
     return undefined;
   }
   return arr[0]
 }
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
