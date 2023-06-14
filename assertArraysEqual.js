9701089ea0115106ef3f1133e04447767baca691
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-14 06:12:46 +0000

    refactoring using require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      return console.log(`Assertion Passed: ${actual}  ===  ${expected}`);
-  
-    } else if (actual !== expected) {
-      return console.log(`Assertion Failed: ${actual}  !==  ${expected}`);
-    }
-  };
-
-  const eqArrays = function(arrayOne,arrayTwo){
-    if (arrayOne.length !== arrayTwo.length) {
-        return false;
-      }
-    
-      for (let i = 0; i < arrayOne.length; i++) {
-        if (arrayOne[i] !== arrayTwo[i]) {
-          return false;
-        }
-      }
-    
-      return true;
-  
-  }
+const eqArrays = require('./eqArrays');
 
   const assertArraysEquals = function(array1, array2){
     if(eqArrays(array1,array2)){
@@ -29,4 +7,6 @@ const assertEqual = function(actual, expected) {
     console.log("Arrays are not equal");
   }
     }
+
+    module.exports = assertArraysEquals;
 
