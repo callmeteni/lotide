d68436305151df04132c559c5dcc2bce0ba4fd25
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-14 05:57:59 +0000

    refactoring using require

--- tail.js
+++ tail.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`Assertion Passed: ${actual}  ===  ${expected}`);
-  
-  } else if (actual !== expected) {
-    return console.log(`Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-};
-  
-// TEST CODE
+const assertEqual = require('./assertEqual');
 
-
-function tail(arr) {
+const tail = function (arr) {
   return arr.slice(1);
 }
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+module.exports = tail;
